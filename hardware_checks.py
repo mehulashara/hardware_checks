d01305a6631b3186a3f58acf6c8498d321f7046c
mehulashara <mehulashara017@gmail.com>
2019-12-28 01:27:07 +0530

    Create hardware_checks.py

---
+++ hardware_checks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#------------------------------------------------------------------------#
+# File name: hardware_checks.py
+# Author: Mehul Ashara  
+# Email: mehul.ashara@nutanix.com
+# Date created: 28/12/2019
+# Date last modifed: 
+# Python version: 2.7.5
+# Purpose: Run NX hardware checks from bootable Phoenix ISO
+# Usage: python 
+# Run ONLY as root user
+#-------------------------------------------------------------------------#
+import subprocess,sys,os
+def smart_disk_checks():
+        disks=os.system("lsscsi|grep disk| awk -F '/' '{print $3}'")
+        print disks
+
+def main():
+        smart_disk_checks()
+
+if __name__ == "__main__":
+        main()
